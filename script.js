d877edeee44f841b4f5a8b4bb05f901afe6559dc
Souvik Guha <svkguha123@gmail.com>
2023-04-27 12:40:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 //complete this code
-class Person {}
 
-class Student extends Person {}
+class Person {
+    constructor(name, age){
+        this.Name = name;
+        this.Age = age;
+    }
+    get name(){
+        return this.Name;
+    }
+    set age(){
+        this.Age = age;
+    }
+    get age(){
+        return this.Age;
+    }
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+    study(){
+        console.log(`${this.Name} is studying`);
+    }
+}
+
+class Teacher extends Person {
+    teach(){
+        console.log(`${this.Age} is teaching`);
+    }
+}
 
 // Do not change the code below this line
 window.Person = Person;
